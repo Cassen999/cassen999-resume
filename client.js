4a5fad7ccadf4e986dfa2e667a3d5c90d747407d
Cassen Gerber <cassenpt@gmail.com>
2020-11-29 16:52:26 -0600

    finished content, working on weekly poll logic

--- client.js
+++ client.js
@@ -3,6 +3,14 @@ $(document).ready(onReady);
 function onReady(params) {
     $('.github').on('click', github)
     $('.linkedin').on('click', linkedin)
+    $('#star-trek').on('click', pollItemOne)
+    $('#star-wars').on('click', pollItemTwo)
+    $('#neither').on('click', pollItemThree)
+    appendDom();
+}
+
+function appendDom() {
+    
 }
 
 function github() {
@@ -11,4 +19,33 @@ function github() {
 
 function linkedin() {
     location.href = "https://linkedin.com/in/cassen-gerber/"
+}
+
+let itemOne = 0;
+let itemTwo = 0;
+let itemThree = 0;
+let pollWinner;
+
+function pollItemOne() {
+    itemOne +=1
+}
+
+function pollItemTwo() {
+    itemTwo +=1
+}
+
+function pollItemThree() {
+    itemThree +=1
+}
+
+function winner() {
+    if (itemOne > itemTwo && itemOne > itemThree) {
+        pollWinner = 'Star Trek'
+    }
+    else if (itemTwo > itemOne && itemTwo > itemThree) {
+        pollWinner = 'Star Wars'
+    }
+    else if (itemThree > itemOne && itemThree > itemTwo) {
+        pollWinner = 'Yuck, neither!'
+    }
 }
