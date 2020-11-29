1cc81316e06c281d266c12f53586fec2fc5148e9
Cassen Gerber <cassenpt@gmail.com>
2020-11-29 17:07:23 -0600

    Working on poll logic still but ran out of time for turning it in

--- client.js
+++ client.js
@@ -1,17 +1,21 @@
+// Working on poll logic but ran out of time, I will still complete this
+
 $(document).ready(onReady);
 
 function onReady(params) {
     $('.github').on('click', github)
     $('.linkedin').on('click', linkedin)
-    $('#star-trek').on('click', pollItemOne)
-    $('#star-wars').on('click', pollItemTwo)
-    $('#neither').on('click', pollItemThree)
-    appendDom();
+    // appendDom();
 }
 
-function appendDom() {
-    
-}
+// function appendDom() {
+//     $('.star-trek').on('click', pollItemOne)
+//     $('.star-wars').on('click', pollItemTwo)
+//     $('.neither').on('click', pollItemThree)
+//     winner();
+//     $('.poll-winner').append(`This week's winner so far is ${pollWinner}!`)
+
+// }
 
 function github() {
     location.href = "https://github.com/Cassen999"
@@ -21,31 +25,43 @@ function linkedin() {
     location.href = "https://linkedin.com/in/cassen-gerber/"
 }
 
-let itemOne = 0;
-let itemTwo = 0;
-let itemThree = 0;
-let pollWinner;
+// let itemOne = 0;
+// let itemTwo = 0;
+// let itemThree = 0;
+// let pollWinner = '';
 
-function pollItemOne() {
-    itemOne +=1
-}
 
-function pollItemTwo() {
-    itemTwo +=1
-}
 
-function pollItemThree() {
-    itemThree +=1
-}
+// function pollItemOne() {
+//     itemOne ++;
+//     console.log(itemOne)
+//     winner()
+// }
 
-function winner() {
-    if (itemOne > itemTwo && itemOne > itemThree) {
-        pollWinner = 'Star Trek'
-    }
-    else if (itemTwo > itemOne && itemTwo > itemThree) {
-        pollWinner = 'Star Wars'
-    }
-    else if (itemThree > itemOne && itemThree > itemTwo) {
-        pollWinner = 'Yuck, neither!'
-    }
-}
+// function pollItemTwo() {
+//     itemTwo ++;
+//     console.log(itemTwo)
+//     winner()
+// }
+
+// function pollItemThree() {
+//     itemThree ++;
+//     console.log(itemThree)
+//     winner()
+// }
+
+// function winner() {
+//     if (itemOne > itemTwo && itemOne > itemThree) {
+//         pollWinner = 'Star Trek'
+//     }
+//     else if (itemTwo > itemOne && itemTwo > itemThree) {
+//         pollWinner = 'Star Wars'
+//     }
+//     else if (itemThree > itemOne && itemThree > itemTwo) {
+//         pollWinner = 'Yuck, neither!'
+//     }
+//     else {
+//         pollWinner = 'No winner yet!'
+//     }
+//     console.log(pollWinner)
+// }
